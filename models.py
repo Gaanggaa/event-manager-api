93140aece1932b36a4698b9d780c616020742b9b
Ganga Gireesh <gireeshganga3@gmail.com>
2025-07-27 14:51:18 +0530

    added all files to new branch

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     location = db.Column(db.String(100), nullable=False)
-    date = db.Column(db.String(50), nullable=False)
+    date = db.Column(db.Date, nullable=False)
 
     attendees = db.relationship('Attendee', backref='event', cascade="all, delete")
 
